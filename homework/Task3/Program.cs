b75b1600269721a6366f9144e7b176a8fad10ea2
Alexandr Vinokurov <salispiligrim@yandex.ru>
2024-03-24 13:28:35 +0100

    add homework:Task3

---
+++ homework/Task3/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+    Напишите программу, которая принимает на вход целое число из отрезка [10, 99] 
+    и показывает наибольшую цифру числа.
+*/
+
+Random randNum = new Random();
+int value = randNum.Next(10, 100);
+Console.WriteLine($"Сравниваем цифры числа: {value}");
+int firstNumValue = value / 10;
+int secondNumValue = value % 10;
+if (firstNumValue > secondNumValue)
+{
+    Console.WriteLine($"первая цифра - {firstNumValue} больше");
+}
+else
+{
+    if (firstNumValue == secondNumValue)
+    {
+        Console.WriteLine($"цифры числа {value} равны");
+    }
+    else
+    {
+        Console.WriteLine($"вторая цифра - {secondNumValue} больше");
+    }
+}
