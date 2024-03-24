b1529982ac2e655330f7d591b5163b8cd7dfd9bb
Alexandr Vinokurov <salispiligrim@yandex.ru>
2024-03-24 12:58:09 +0100

    add homework:Task1

---
+++ homework/Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+    Напишите программу, которая принимает на вход число и проверяет
+    кратно ли оно одновременно 7 и 23
+
+    Примеры:
+    50 => Нет
+    7 => Нет
+    322 => Да
+*/
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 7 == 0 && number % 23 == 0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
