817c2265e6772481911f279c0499c73922016d69
Alexandr Vinokurov <salispiligrim@yandex.ru>
2024-03-24 14:26:46 +0100

    add homework:Task4

---
+++ homework/Task4/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+    Напишите программу, которая на вход принимает натуральное число N, 
+    а на выходе показывает его цифры через запятую.
+*/
+Console.WriteLine("Введите натуральное число");
+int value = Convert.ToInt32(Console.ReadLine());
+int writeWalue = value;
+while (writeWalue > 0)
+{
+    Console.Write($"{writeWalue % 10}");
+    if (writeWalue > 9)
+    {
+        Console.Write(", ");
+    }
+    writeWalue = writeWalue / 10;
+}
